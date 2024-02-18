72bff30b0871d0f1439e475824ff24e563b52f7f
Andrej <andrej@case.edu>
2024-02-17 21:55:26 -0500

    Turns out we *can* use a hashtable.

--- interpreter.rkt
+++ interpreter.rkt
@@ -2,7 +2,6 @@
 
 (require "simpleParser.rkt")
 
-;recursively running the program. Each statement is divided into an element of a list by parser.
 (define statementHandler
     (lambda (prog state)
         (cond   
@@ -16,25 +15,74 @@
 
 (define lookup
     (lambda (state key)
-        (cond
-            ;[Daniel]: it is normal for return not to find a value, so just return null here. Error should be handled
-            ;in declare and assign separately if they find null.  
-            [(null? state) null]
-            [(eq? (car (car state)) key) (car (cdr (car state)))]
-            [else (lookup (cdr state) key)])
-    )
-)
+        (begin
+            (display "Looking up key: ") (display key) (newline)
+            (if (hash-has-key? state key)
+                (hash-ref state key)
+                '()))))
 
 (define M_state
     (lambda (statement state)
-        (cond
-            [(isReturn? statement)          (return statement state)]
-            [(isDeclaration? statement)     (declare statement state)]
-            [(isAssignment? statement)      (assign statement state)]
-            [(isIfStatement? statement)     (ifImp statement state)]
-            [(isWhileStatement? statement)  (whileImp statement state)]
-            [else (error "Invalid statement")]
-    )))
+        (begin
+            (display "Running M_state with statement: ") (display statement) (newline)
+            (cond
+                [(isReturn? statement)          (return statement state)]
+                [(isDeclaration? statement)     (declare statement state)]
+                [(isAssignment? statement)      (assign statement state)]
+                [(isIfStatement? statement)     (ifImp statement state)]
+                [(isWhileStatement? statement)  (whileImp statement state)]
+                [else (error "Invalid statement")]
+            ))))
+
+(define return 
+    (lambda (statement state)
+        (begin
+            (display "Running return with statement: ") (display statement) (newline)
+            (hash-set state 'return (M_value (cadr statement) state)))))
+
+(define declare
+    (lambda (statement state)
+        (begin
+            (display "Running declare with statement: ") (display statement) (newline)
+            (hash-set state (cadr statement) 0))))
+
+(define assign
+    (lambda (statement state)
+        (begin
+            (display "Running assign with statement: ") (display statement) (newline)
+            (hash-set state (cadr statement) (M_value (caddr statement) state)))))
+
+(define ifImp
+    (lambda (statement state)
+        (begin
+            (display "Running ifImp with statement: ") (display statement) (newline)
+            (if (M_boolean (cadr statement) state)
+                (statementHandler (caddr statement) state)
+                (statementHandler (cadddr statement) state)))))
+
+(define whileImp
+    (lambda (statement state)
+        (begin
+            (display "Running whileImp with statement: ") (display statement) (newline)
+            (if (M_boolean (cadr statement) state)
+                (begin (statementHandler (caddr statement) state)
+                       (whileImp statement state))
+                '()))))
+
+(define M_value
+    (lambda (statement state)
+        (begin
+            (display "Running M_value with statement: ") (display statement) (newline)
+            (cond
+                [(number? statement)        statement]
+                [(symbol? statement)        (lookup state statement)]
+                [(eq? (car statement) '+)   (+ (M_value (cadr statement) state) (M_value (caddr statement) state))]
+                [(eq? (car statement) '-)   (- (M_value (cadr statement) state) (M_value (caddr statement) state))]
+                [(eq? (car statement) '*)   (* (M_value (cadr statement) state) (M_value (caddr statement) state))]
+                [(eq? (car statement) '/)   (quotient (M_value (cadr statement) state) (M_value (caddr statement) state))]
+                [(eq? (car statement) '%)   (remainder (M_value (cadr statement) state) (M_value (caddr statement) state))]
+                [else (lookup state statement)]
+            ))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
@@ -50,59 +98,22 @@
 
 ;[Daniel]: In my view, the return function doesn't 'return' the value. Instead, it store the value into state 
 ; and StatementHandler could return it in the next recurively call.  
-(define return 
-    (lambda (statement state)
-        (cons state (cons 'return M_value(statement state)))))
 
 (define isDeclaration?
     (lambda (statement)
-        (eq? (car statement) 'declare)))
-
-(define declare
-    (lambda (statement)
-        (set (cadr statement) 0)))
+        (and (list? statement) (eq? (car statement) 'var))))
 
 (define isAssignment?
     (lambda (statement)
-        (eq? (car statement) 'var)))
-
-(define assign
-    (lambda (statement state)
-        (set (cadr statement) (M_value (caddr statement) state))))
+        (eq? (car statement) '=)))
 
 (define isIfStatement?
     (lambda (statement)
-        (eq? (car statement) 'if)))
-
-(define ifImp
-    (lambda (statement)
-        (if (M_boolean (cadr statement))
-            (statementHandler (caddr statement))
-            (statementHandler (cadddr statement)))))
+        (and (list? statement) (eq? (car statement) 'if))))
 
 (define isWhileStatement?
     (lambda (statement)
-        (eq? (car statement) 'while)))
-
-(define whileImp
-    (lambda (statement)
-        (if (M_boolean (cadr statement))
-            (begin (statementHandler (caddr statement))
-                   (whileImp statement))
-            '())))
-
-(define M_value
-    (lambda (statement state)
-        (cond
-            [(number? statement)        statement]
-            [(not (null? (lookup statement state))) (lookup statement state)];if it is a variable 
-            [(eq? (car statement) '+)   (+ (M_value (cadr statement) state) (M_value (caddr statement) state))]
-            [(eq? (car statement) '-)   (- (M_value (cadr statement) state) (M_value (caddr statement) state))]
-            [(eq? (car statement) '*)   (* (M_value (cadr statement) state) (M_value (caddr statement) state))]
-            [(eq? (car statement) '/)   (quotient (M_value (cadr statement) state) (M_value (caddr statement) state))]
-            [(eq? (car statement) '%)   (remainder (M_value (cadr statement) state) (M_value (caddr statement) state))]
-            [else (error "not a value")]
-        )))
+        (and (list? statement) (eq? (car statement) 'while))))
 
 ;TODO: we shouldn't return #t or #f but true or false here.
 (define M_boolean 
@@ -112,7 +123,7 @@
             [(eq? (car statement) '>=) (>= (M_value (cadr statement) state) (M_value (caddr statement) state))]
             [(eq? (car statement) '<)  (< (M_value (cadr statement) state) (M_value (caddr statement) state))]
             [(eq? (car statement) '<=) (<= (M_value (cadr statement) state) (M_value (caddr statement) state))]
-            [(eq? (car statement) '=)  (= (M_value (cadr statement) state) (M_value (caddr statement) state))]
+            [(eq? (car statement) '=)  (assign (cadr statement) (M_value (caddr statement) state)) (M_value (cadr statement) state)]
             [(eq? (car statement) '!=) (not (= (M_value (cadr statement) state) (M_value (caddr statement) state)))]
             [(eq? (car statement) '&&) (and (M_boolean (cadr statement) state) (M_boolean (caddr statement) state))]
             [(eq? (car statement) '||) (or (M_boolean (cadr statement) state) (M_boolean (caddr statement) state))]
@@ -121,4 +132,4 @@
 
 
 (parser "../CSDS345/proj1/test1.txt")
-(statementHandler (parser "../CSDS345/proj1/test1.txt") '())
+(statementHandler (parser "../CSDS345/proj1/test1.txt") (hash))
