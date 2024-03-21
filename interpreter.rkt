ad33fa54e2a1155a4b80b1a458479b169be7ada3
Andrej <andrej@case.edu>
2024-03-20 20:48:21 -0400

    Attempts to implement variable scoping

--- interpreter.rkt
+++ interpreter.rkt
@@ -6,6 +6,90 @@
 
 (require "simpleParser.rkt") 
 
+; Evaluation abstractions
+(define pre_op car)
+(define l_operand cadr)
+(define r_operand caddr)
+
+; State access abstractions
+(define state_vars car)
+(define state_vals cadr)
+
+; Variable access abstractions
+(define var_name cadr)
+(define var_value caddr)
+
+; If-statement & while-loop abstractions
+(define condition cadr)
+(define stmt1 caddr)
+(define elif cdddr)
+(define stmt2 cadddr)
+(define loop_body cddr)
+
+; Statement list abstractions
+(define curr_stmt car)
+(define next_stmt cdr)
+(define curr_inner_stmt caar)
+(define finally_block cadddr)
+(define catch_block caddr)
+(define catch_var caaddr)
+(define try_block cadr)
+(define throw_block cadr)
+
+; Return abstraction
+(define ret_val cadr)
+
+; Empty state
+(define empty_state '(()()))
+
+; Checks if an atom is a potential variable name.
+(define var?
+  (lambda (x)
+    (not (or (pair? x) (null? x)))))
+
+; Retrieves the value of a given variable.
+; Here, the state takes the form ((var1 var2 var3 ...) (val1 val2 val3 ...) [nested states here]).
+(define find_var
+  (lambda (var state)
+    (cond
+      [(equal? state empty_state) (error 'error_var_not_declared "Variable not declared: ~a" var)]
+      [(null? (state_vars state)) (find_var var (pop_inner_state state))]
+      [(and (eq? var (car (state_vars state))) (void? (unbox (car (state_vals state))))) (error 'error_var_not_assigned "Variable not assigned: ~a" var)]
+      [(eq? var (car (state_vars state))) (unbox (car (state_vals state)))]
+      [else (find_var var (cons (cdr (state_vars state)) (cons (cdr (state_vals state)) (cddr state))))])))
+
+; Adds a variable to the state and returns the state.
+; If the variable already exists in the state, then raise an error.
+(define add_var
+  (lambda (var val state)
+    (cond
+      [(declared? var state) (error 'error_var_doubledeclare "Variable already declared: ~a" var)]
+      [(null? (cddr state)) (list (cons var (state_vars state)) (cons (box val) (state_vals state)))]
+      [else (list (cons var (state_vars state)) (cons (box val) (state_vals state)) (pop_inner_state state))])))
+
+(define declared?
+  (lambda (var state)
+    (cond
+      [(equal? state empty_state) #f]
+      [(null? (state_vars state)) #f]
+      [(eq? var (car (state_vars state))) #t]
+      [else (declared? var (cons (cdr (state_vars state)) (cons (cdr (state_vals state)) (cddr state))))])))
+
+; Assigns a particular value to a given variable.
+; This utilizes set-box!, which will cause side effects by default.
+(define assign_var!
+  (lambda (var value state)
+    (call/cc (lambda (end) (assign_var_helper! var value state state)))))
+
+(define assign_var_helper!
+  (lambda (var value state end)
+    (cond
+      [(equal? state empty_state) (error 'error_var_not_declared "Variable not declared: ~a" var)]
+      [(null? (state_vars state)) (assign_var_helper! var value (pop_inner_state state) end)]
+      [(eq? var (car (state_vars state))) (begin (set-box! (car (state_vals state)) value) end)]
+      [else (assign_var_helper! var value (cons (cdr (state_vars state)) (cons (cdr (state_vals state)) (cddr state))) end)])))
+
+
 (define (statementHandler prog state k)
   (if (null? prog) 
       (k state)
@@ -14,18 +98,26 @@
 ; TODO: lookup is called too many times. 
 ; an example of state is '((x 10) (y 9)). caar access 'x, and cadar access '10.
 ; [Daniel]: change to '((x y) (10 9)). (car state) access key, and (cadr state) access value.
-; TODO: working on 'list of layers' that support local variables.  
-(define (lookup state key)
-    (if(null? state) 
-        null
-        (lookup-rec (car state) (cadr state) key)))
+; scopes is now a list of lists, where each list is a scope.
+(define (lookup state var)
+; If the "variable" is actually a statement, then we need to evaluate it.
+  (if (and (list? var) (not (symbol? var)))
+      (M_value var state (lambda (v) v))
+      ; Otherwise, we can just look it up.
+      (find_var var state))
+)
 
-(define (lookup-rec state-key state-val key)
-    (cond 
-        [(null? state-key)              null]
-        [(eq? (car state-key) key)      (car state-val)]
-        [else                           (lookup-rec (cdr state-key) (cdr state-val) key)]
-    ))
+; Creates a new layer for the state.
+(define create_inner_state
+  (lambda (state)
+    (cons '() '() state)))
+
+; Pops the newest layer from the state.
+(define pop_inner_state
+  (lambda (state)
+    (if (null? (cddr state))
+        (error 'error_illegal_break "Cannot break here.")
+        (caddr state))))
 
 (define (isReturn? statement)
         (eq? (car statement) 'return))
@@ -71,38 +163,41 @@
             [else (error "Invalid return value")]))
 
 ; [Daniel]: now it return the index of key. This is passed as a parameter in addBinding to remove and re-add binding.
-(define (searchBinding state_key var)
-    (cond
-        [(null? state_key)                 (error "Key not found in removing binding.")]
-        [(eq? (car state_key) var)         0]
-        [else                              (+ 1 (searchBinding (cdr state_key) var))]))
+(define (searchBinding scopes var)
+    (if (null? scopes)
+            (error "Key not found in removing binding.")
+            (let ((index (findIndex (car scopes) var)))
+                (if index
+                        index
+                        (searchBinding (cdr scopes) var)))))
 
-(define (addBinding state key val pos)
-    (list (addBinding-rec (car state) key pos) (addBinding-rec (cadr state) val pos)))
+(define (findIndex scope var)
+    (let loop ((scope scope) (index 0))
+        (cond
+            [(null? scope) null]
+            [(eq? (caar scope) var) index]
+            [else (loop (cdr scope) (+ index 1))])))
 
-(define (addBinding-rec state value k)
-    (cond
-        [(eq? k 'front)                         (cons value state)]
-        [(and (eq? k 0) (null? state))          (cons value '())]
-        [(eq? k 0)                              (cons value (cdr state))]
-        [else                                   (cons (car state) (addBinding-rec (cdr state) value (- k 1)))]
-    ))
+(define (addBinding scopes key val)
+  (add_var key val scopes))
+
+(define (updateBinding scopes key val index)
+  (assign_var! key val scopes))
+
+(define (updateScope scope key val index)
+    (if (eq? index 0)
+            (cons (cons key val) (cdr scope))
+            (cons (car scope) (updateScope (cdr scope) key val (- index 1)))))
 
 (define (breakImp statement state)
   ; Implementation of break statement
-    (if (null? (lookup state 'break))
-        (error "Break statement outside of loop")
-        (addBinding state 'break 'true 'front)
-    )
-  )
+  (error "Break statement not implemented yet")
+)
 
 (define (continueImp statement state)
-    (if (null? (lookup state 'continue))
-        (error "Continue statement outside of loop")
-        (addBinding state 'continue 'true 'front)
-    )
-  ; Implementation of continue statement
-  )
+    ; Implementation of continue statement
+    (error "Continue statement not implemented yet")
+)
 
 (define (tryImp statement state next)
   (call/cc
@@ -112,7 +207,7 @@
            (lambda (e)
              (if (null? (caddr statement))
                  (exit state)
-                 (M_state (caddr statement) (addBinding state 'e e 'front) (lambda (v) v) (lambda (v) v))))])
+                 (M_state (caddr statement) (addBinding state 'e e) (lambda (v) v) (lambda (v) v))))])
        (M_state (cadr statement) state (lambda (v) v) (lambda (v) v))
        (if (not (null? (cadddr statement)))
               (M_state (cadddr statement) state (lambda (v) v) (lambda (v) v))
@@ -121,25 +216,24 @@
 (define (throwImp statement state)
     (if (null? (lookup state 'e))
         (error "Throw statement outside of try block")
-        (addBinding state 'e (lookup state 'e) 'front)
+        (addBinding state 'e (lookup state 'e))
     )
 )
 
 (define (assign statement state)
-    (if (null? (lookup state (cadr statement)))                
-        (error "Variable is not declared")
-        (addBinding state (cadr statement) (M_value (caddr statement) state (lambda(v) v))
-            (searchBinding (car state) (cadr statement)))
-    )
+  (if (null? (lookup state (cadr statement)))                
+      (error "Variable is not declared")
+      (assign_var! (cadr statement) (M_value (caddr statement) state (lambda(v) v)) state)
+  )
 )
 
 (define (declare statement state)
-            (if (not (null? (lookup state (cadr statement))))
-                (error "Variable is already declared")
-                ;whether the variable is given a initial value
-                (if (null? (cddr statement))
-                    (addBinding state (cadr statement) 'value_undefined 'front)
-                    (addBinding state (cadr statement) (M_value (caddr statement) state (lambda(v) v)) 'front))))
+  (if (declared? (cadr statement) state)
+      (error "Variable is already declared")
+      ;whether the variable is given a initial value
+      (if (null? (cddr statement))
+          (add_var (cadr statement) 'value_undefined state)
+          (add_var (cadr statement) (M_value (caddr statement) state (lambda(v) v)) state))))
 
 (define (ifImp statement state)
             (if (M_value (cadr statement) state (lambda(v) v))
@@ -161,10 +255,10 @@
         (M_state body state (lambda(s1) (loop condition body s1 next break)) break)
         (next state)))
 
-(define (beginImp statement state break)
-    (if (null? statement)
-        state
-        (beginImp (cdr statement) (M_state (car statement) state (cdr statement) break) break)))
+(define (beginImp statements state next break)
+    (if (null? statements)
+            (next (pop_inner_state state)) ; pop the current scope off the stack
+            (beginImp (cdr statements) (M_state (car statements) state next break) next break)))
 
 (define (catchImp statement state)
     (if (null? (lookup state 'e))
@@ -184,34 +278,35 @@
     [(isAssignment? statement)      (assign statement state)]
     [(isIfStatement? statement)     (ifImp statement state)]
     [(isWhileStatement? statement)  (whileImp statement state next break)]
-    [(isBeginStatement? statement)  (beginImp (cdr statement) state next)]
+    [(isBeginStatement? statement) (beginImp (cdr statement) (create_inner_state state) next break)] ; create a new scope
     [else (error "Invalid statement")]
   ))
 
 (define (M_value statement state k)
   (cond
-    [(number? statement)                                        (k statement)]
-    [(eq? statement 'false)                                     (k #f)]
-    [(eq? statement 'true)                                      (k #t)]
-    [(eq? (lookup state statement) 'value_undefined)            (error "Variable is not assigned a value")]
+    [(list? statement) (map (lambda (s) (M_value s state k)) statement)]
+    [(number? statement) (k statement)]
+    [(var? statement) (k (find_var statement state))]
+    [(eq? statement 'false) (k #f)]
+    [(eq? statement 'true) (k #t)]
+    [(and (eq? (car statement) '-) (null? (cddr statement))) (M_value (cadr statement) state (lambda (v) (k (* v -1))))]
+    [(eq? (car statement) '-) (k (- (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(eq? (car statement) '+) (k (+ (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(eq? (car statement) '*) (k (* (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(eq? (car statement) '/) (k (/ (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(eq? (car statement) '%) (k (modulo (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(eq? (car statement) '>) (k (> (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(eq? (car statement) '>=) (k (>= (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(eq? (car statement) '<) (k (< (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(eq? (car statement) '<=) (k (<= (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(eq? (car statement) '!=) (k (not (eq? (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v)))))]
+    [(eq? (car statement) '!) (k (not (M_value (cadr statement) state (lambda (v) v ))))]
+    [(eq? (car statement) '&&) (k (and (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(eq? (car statement) '||) (k (or (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(eq? (car statement) '==) (k (eq? (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
     [(and (symbol? statement) (null? (lookup state statement))) (error "Variable is not declared")]
-    [(symbol? statement)                                        (k (lookup state statement))]
-    ;special condition when '- acts as negative sign
-    [(and (eq? (car statement) '-) (null? (cddr statement)))    (M_value (cadr statement) state (lambda (v) (k (* v -1))))]
-    [(eq? (car statement) '-)                                  (k (- (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
-    [(eq? (car statement) '+)                                   (k (+ (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
-    [(eq? (car statement) '*)                                   (k (* (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
-    [(eq? (car statement) '/)                                   (k (/ (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
-    [(eq? (car statement) '%)                                   (k (modulo (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
-    [(eq? (car statement) '>)                                   (k (> (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
-    [(eq? (car statement) '>=)                                  (k (>= (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
-    [(eq? (car statement) '<)                                   (k (< (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
-    [(eq? (car statement) '<=)                                  (k (<= (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
-    [(eq? (car statement) '!=)                                  (k (not (eq? (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v)))))]
-    [(eq? (car statement) '!)                                   (k (not (M_value (cadr statement) state (lambda (v) v ))))]
-    [(eq? (car statement) '&&)                                  (k (and (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
-    [(eq? (car statement) '||)                                  (k (or (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
-    [(eq? (car statement) '==)                                  (k (eq? (M_value (cadr statement) state (lambda (v) v)) (M_value (caddr statement) state (lambda (v) v))))]
+    [(symbol? statement) (k (lookup state statement))]
+    [(eq? (lookup state statement) 'value_undefined) (error "Variable is not assigned a value")]
     [else (error "Invalid value")]
 ))
 
